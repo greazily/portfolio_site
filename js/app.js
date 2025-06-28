7d65d4a32ccccdb790955db8bb334dcf857d6404
full-bleed <dahlen.mikael@gmail.com>
2025-06-28 23:58:11 +0200

    Need to fix paragraph change

--- js/app.js
+++ js/app.js
@@ -99,8 +99,17 @@ function infoSet(index, column) {
   let inactive = document.querySelectorAll(".project");
   let active = document.getElementById("n" + index);
   let paragraph = document.querySelector(".information div p");
-  const insights = ["Hunterbrook info", "Sony Group info", "Data Desk info", "Alber Elbaz info", "MDX University info", "Miscellaneous info"];
-  const details = "Details info";
+  const insights = [
+    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.", 
+    "No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.", 
+    "Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.", 
+    "To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?", 
+    "But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?", 
+    "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain."
+  ];
+  const details = 
+  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
+  ;
 
   if(column == 1){
     paragraph.innerHTML = details;
